9c9dc686a9a60d1b235d74fffcb7a5ec627c5e0a
Jorge Suárez <jorgealejandrosuarez0807@gmail.com>
2024-06-17 09:43:56 -0400

    comienzan pruebas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
+	content: ["./src/**/*.{html,js}", "./public/*.{html,js}"],
+	theme: {
+		fontFamily: {
+			Montserrat: ['"Montserrat"', "sans-serif"],
+		},
+		colors: {
+			primary: "#CC2D4A",
+			secondary: "#8FA206",
+			tertiary: "#61AEC9",
+		},
+		extend: {
+      backgroundImage : {
+        sanFrancisco: "url('../img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+        yosemite: "url('../img/yosemite.jpg')",
+        LA: "url('../img/LA.jpg')",
+        seattle: "url('../img/seattle.jpg')",
+        new_york: "url('../img/new_york.jpg')",
+        norway: "url('../img/norway.jpg')",
+        sydney: "url('../img/sydney.jpg')",
+        miami: "url('../img/miami.jpg')",
+        switzerland: "url('../img/switzerland.jpg')",
+        bali: "url('../img/bali.jpg')",
+        norway: "url('../img/norway.jpg')",
+        chicago: "url('../img/chicago.jpg')",
+        europe: "url('../img/europe.jpg')",
+        iceland: "url('../img/iceland.jpg')",
       }
-    }
-  }
-}
+		},
+	},
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/typography"),
+		require("@tailwindcss/aspect-ratio"),
+	],
+};
